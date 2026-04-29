614a754f33528e55508fddaf9d54e138807814c9
Dmitrii Nikulin <nikdim_03@mail.ru>
2026-04-29 22:49:46 +0800

    chore: spotless

--- AndroidDemo/app/src/main/java/io/ton/walletkit/demo/presentation/ui/icons/DemoIcons.kt
+++ AndroidDemo/app/src/main/java/io/ton/walletkit/demo/presentation/ui/icons/DemoIcons.kt
@@ -19,6 +19,8 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+@file:Suppress("ktlint:standard:backing-property-naming")
+
 package io.ton.walletkit.demo.presentation.ui.icons
 
 import androidx.compose.material.icons.Icons
